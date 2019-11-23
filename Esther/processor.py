a743b377f2abba026a79a2bd6176422630200ad5
Azoor <31021840+azoor-guy@users.noreply.github.com>
2019-11-23 13:12:46 +0800

    Fixed bug when result probability is > 1 + Cleanup
    - Edited outlines list layout to:
    distMod tuple for each phrase
    contain phraseWeights for each phrase, replace noKeywords
    removed probability
    
    - Removed keywords list, replaced by phraseWeight
    - Rearranged the passed variables for AddOutlines() to be more consistent

--- Esther/processor.py
+++ Esther/processor.py
@@ -9,7 +9,6 @@ MOD_PATH = os.path.join(APP_PATH, "modules")
 
 class Processor():
     outlines = {}
-    keywords = {}
     synonyms = {}
     entitiesList = {}
 
@@ -17,26 +16,23 @@ class Processor():
         print ("Initializing processor...")
         this.LoadAllModules()
 
-        this.keywords.setdefault("ask_time",["time"])
-        this.keywords.setdefault("ask_if_faggot",["faggot"])
-
         this.synonyms.setdefault("require","need")
         this.synonyms.setdefault("pc","computer")
         this.synonyms.setdefault("rig","computer")
         this.synonyms.setdefault("battlestation","computer")
+        this.synonyms.setdefault("battle station","computer")
 
         this.entitiesList.setdefault("!days",["monday","tuesday","wednesday","thursday","friday","saturday","sunday"])
         this.entitiesList.setdefault("!days_relative",["yesterday","today","tomorrow"])
 
-        this.AddOutline(["what","time"], "ask_time", 1,0.1)
-        this.AddOutline(["give","time"], "ask_time", 1,0.1)
-        this.AddOutline(["tell","me","time"], "ask_time", 1,0.1)
-        this.AddOutline(["need","time"], "ask_time", 1,0.1)
-        this.AddOutline(["do","know","time"], "ask_time", 1,0.1)
-        this.AddOutline(["have","current","time"], "ask_time", 1,0.15)
-        this.AddOutline(["hello","you","faggot"], "ask_if_faggot", 1,0.1)
-        this.AddOutline(["time"], "ask_time", 0.8,0)
-        this.AddOutline(["are","you","faggot"], "ask_if_faggot", 1,0.3)
+        this.AddOutline("ask_time", ["what","time"],(0,0.1),(1,2))
+        this.AddOutline("ask_time", ["give","time"],(0,0.1),(1,2))
+        this.AddOutline("ask_time", ["tell","me","time"],(0,1,0.1),(1,1,2))
+        this.AddOutline("ask_time", ["need","time"],(0,0.1),(1,2))
+        this.AddOutline("ask_time", ["do","know","time"],(0,0.1,0.1),(1,1,2))
+        this.AddOutline("ask_time", ["have","current","time"],(0,0.1,0.1),(1,1,2))
+        this.AddOutline("ask_if_faggot", ["are","you","faggot"],(0,0.1,0.1),(1,1,2))
+        this.AddOutline("ask_if_faggot", ["hello","you","faggot"],(0,0.1,0.1),(1,1,2))
 
         print ("Processor initialized!\n")
         
@@ -60,21 +56,24 @@ class Processor():
 
         #outline structure
         # "phrase[0]" : [
-        #("intentName", probability, distMod, [phrase], noKeywords),
-        #("intentName", probability, distMod, [phrase], noKeywords),
-        #("intentName", probability, distMod, [phrase], noKeywords)
-        # ]
-    def AddOutline (this, phraseList, intentName, probability, distMod):
-        #find number of keywords in phrases
-        noKeywords = 0
-        if intentName in this.outlines:
-            for item in phraseList:
-                if item in this.keywords[intentName]:
-                    noKeywords += 1
-        if phraseList[0] in this.outlines:
-            this.outlines.get(phraseList[0]).append((intentName,probability, distMod, phraseList, noKeywords))
+        #("intentName", [phrases], [distMod], [phraseWeight]),
+        #("intentName", [phrases], [distMod], [phraseWeight]),
+        #("intentName", [phrases], [distMod], [phraseWeight])
+        # ] 
+    def AddOutline (this, intentName, phrases, distMod, phraseWeight):
+        #phraseWeight is in [1,1,2,1] format, need to convert such that they all add up to 1.
+        totalWeightRaw = 0
+        newTuple = [0,]* len(phraseWeight)
+
+        for rawWeight in phraseWeight:
+            totalWeightRaw += rawWeight #get sum of all weights
+        for i in range(0,len(phraseWeight)):
+            newTuple[i] = (phraseWeight[i] / totalWeightRaw) #set new value into each tuple.
+
+        if phrases[0] in this.outlines:
+            this.outlines.get(phrases[0]).append((intentName, phrases, distMod, newTuple))
         else:
-            this.outlines.setdefault(phraseList[0],[]).append((intentName,probability, distMod, phraseList, noKeywords))
+            this.outlines.setdefault(phrases[0],[]).append((intentName, phrases, distMod, newTuple))
 
     def ProcessInput(this,_usrinput):
         #clean up the user input. Remove all punctuations, leaving only . - and '
@@ -103,7 +102,7 @@ class Processor():
 
         #Iterate every word in user sentence
         for i in range(0,len(usrinput)):
-            print ("Now checking: " + usrinput[i])
+
             #if current user word matches keyword
             if usrinput[i] in this.outlines:
 
@@ -111,47 +110,36 @@ class Processor():
                 for nestedOutline in this.outlines.get(usrinput[i]):
 
                     #create tracker for which position each phrase appears in user's sentence
-                    print (nestedOutline)
-                    phrasePos = [-1,] * len(nestedOutline[3])
+                    print ("Recognized intent : " + str(nestedOutline))
+                    phrasePos = [-1,] * len(nestedOutline[1])
 
                     #Start to scan the rest of the sentence using each outline in outlines[item]
                     #Start scanning from detected word onwards, not full sentence.
                     for j in range(i,len(usrinput)):
-                        if usrinput[j] in nestedOutline[3]:
-                            phrasePos[nestedOutline[3].index(usrinput[j])] = j
-                    print (phrasePos)
+                        if usrinput[j] in nestedOutline[1]:
+                            phrasePos[nestedOutline[1].index(usrinput[j])] = j
+                    print ("Phrase's position in user sentence: " + str(phrasePos))
 
                     #Calculating scores
-                    #1 divide score by amount of phrases (if 4 phrases, each word will be worth 0.25)
-                    #Keywords will hold more weight. Set in the intent txt file.
-                    #Weighted values: 0 means hold no weight, 1 means normal weight, 2 means twice the weight.
-                    #2 Use distMod. If not the first phrase, do this: phraseBaseScore - ((currindex - previndex - 1) * distMod * phraseBaseScore)
-                    #distMod will range from 0 (length dosent matter) to 1 (100% score penalty for each length away. 2 words means 200% penalty.)
-                    #3 Use probability to round off the shiz. Default will be 1.
+                    #Take phraseWeight and apply below formula.
+                    #If not the first phrase, do this: phraseBaseScore - ((currindex - previndex - 1) * distMod * phraseWeight)
+                    #distMod will range from 0 (no penalty) to 1 (100% score penalty for each length away. 2 words means 200% penalty.)
+                    #add all scores together
                     
                     outlineScore = 0
-                    keywordWeight = 1.7;
-
-                    #Calculate totalScore taking note of keywords
-                    totalScore = len(phrasePos) - nestedOutline[4] + keywordWeight * nestedOutline[4]
 
                     for k in range(0,len(phrasePos)):
                         #if this position index is not -1, it has a legit position in user's sentence. 
                         #If not, phrase does not exist in user's sentence, so do not calculate.
                         if phrasePos[k] != -1:
-                            #Calculate the base score of each phrase
-                            if nestedOutline[3][k] in this.keywords[nestedOutline[0]]:
-                                phraseBaseScore = 1/totalScore*keywordWeight
-                            else:
-                                phraseBaseScore = 1/totalScore*1
                             #use base score to calculate modifier for words apart, and then add onto final score for this outline
                             if k != 0:
-                                outlineScore += phraseBaseScore - ((abs(phrasePos[k] - phrasePos[k-1]) - 1) * phraseBaseScore * nestedOutline[2])
+                                if phrasePos[k] - phrasePos[k-1] > 0: #if the position diff is -ve, e.g. [time, need] instead of [need, time], SKIP. DO NOT ADD ANY SCORE.
+                                    outlineScore += nestedOutline[3][k] - ((phrasePos[k] - phrasePos[k-1] - 1) * nestedOutline[2][k] * nestedOutline[3][k])
                             else:
-                                outlineScore += phraseBaseScore
+                                outlineScore += nestedOutline[3][k]
 
-                    outlineScore *= nestedOutline[1]
-                    print (outlineScore)
+                    print ("Score for this outline: " + str(outlineScore))
 
                     #if scores alr exists, use the higher score
                     if nestedOutline[0] in scores:
@@ -159,4 +147,4 @@ class Processor():
                             scores[nestedOutline[0]] = outlineScore
                     else:
                         scores.setdefault(nestedOutline[0],outlineScore)
-        print (scores)
+        print ("Final Results: " + str(scores))
